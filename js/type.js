1ab6c2d22c13c34d5ed4f1df1ef4970ce6fd3b52
rakshit <rdsonnn@gmail.com>
2020-11-18 16:44:27 +0530

    typing effect in explore

---
+++ js/type.js
@@ -0,0 +1,36 @@
+var typed = new Typed('.type', {
+        
+    strings: ['TravelSpots',
+        'Restaurents',
+
+    ],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+
+
+// 2nd type
+// var typed = new Typed('.type', {
+//     strings: ['Some <i>strings</i> with', 'Some <strong>HTML</strong>', 'Chars &times; &copy;'],
+//     typeSpeed: 0,
+//     backSpeed: 0,
+//     fadeOut: true,
+//     loop: true
+//   });
+
+
+
+//3rd type
+
+// var typed5 = new Typed('#typed5', {
+//     strings: ['1 Some <i>strings</i> with', '2 Some <strong>HTML</strong>', '3 Chars &times; &copy;'],
+//     typeSpeed: 0,
+//     backSpeed: 0,
+//     cursorChar: '_',
+//     shuffle: true,
+//     smartBackspace: false,
+//     loop: true
+//   });
+    
